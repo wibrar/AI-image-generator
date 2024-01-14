97795ee5bb5c27c3f2af8a18c274bb8987e0e56b
wasif <wibrar@mun.ca>
2024-01-14 05:34:34 -0330

    structure of App

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, TextInput, View, Text, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [imageBase64, setImageBase64] = useState(null);
+  const [input, setInput] = useState("");
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter text to convert it into an image</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setInput(text)}
+        value={input}
+      />
+      <TouchableOpacity
+        onPress={getImage}
+        style={styles.buttonStyle}
+      >
+        <Text style={styles.buttonText}>Click to generate</Text>
+      </TouchableOpacity>
+      {imageBase64 && (
+        <Image
+          source={{ uri: imageBase64 }}
+          style={styles.image}
+        />
+      )}
       <StatusBar style="auto" />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
